4b1282ecb81dd921afdbaeaf46015be9bbee4d9f
Derek Nola <derek.nola@suse.com>
2026-08-28 11:10:02 -0700

    Partial reversion of https://github.com/rancher/image-build-crictl/pull/91, use the golang version for the latest patch of the minor, no from v1.XX.0
    Signed-off-by: Derek Nola <derek.nola@suse.com>

--- scripts/golang-version.sh
+++ scripts/golang-version.sh
@@ -1,13 +1,11 @@
 #!/usr/bin/env bash
 
-set -x
-
 cd $(dirname $0)
 
 which yq > /dev/null || go install github.com/mikefarah/yq/v4@v4.35.2
 
-K8S_VERSION=$(./semver-parse.sh $1 all)
-
+TAG_MINOR=$(./semver-parse.sh $1 minor)
+K8S_VERSION=$(curl -sL https://proxy.golang.org/k8s.io/kubernetes/@v/list | grep -v - | grep v1.${TAG_MINOR} | sort -V | tail -n 1)
 if [ -z "${K8S_VERSION}" ] || [ "${K8S_VERSION}" == "v.." ]; then
   echo "No Kubernetes version found in tag ${1}"
   exit 1
@@ -15,7 +13,6 @@ fi
 
 GO_VERSION_URL="https://raw.githubusercontent.com/kubernetes/kubernetes/${K8S_VERSION}/.go-version"
 GO_VERSION=$(curl -sL "${GO_VERSION_URL}")
-
 if [[ "${GO_VERSION}" != "1."* ]]; then
   echo "No Go version found for Kubernetes ${K8S_VERSION}"
   exit 1
@@ -30,7 +27,9 @@ TAG=""
 while [ -n "${NEXT_URL}" ] && [ $PAGE -lt $MAX_PAGE ]; do
   RESPONSE=$(curl -s "$NEXT_URL")
   NEXT_URL=$(echo "$RESPONSE" | yq -r '.next // ""')
+  set -x
   TAGS=$(echo "$RESPONSE" | yq -r '.results[].name')
+  set +x
   TAG=$(echo "${TAGS}" | grep "${GO_VERSION}b[0-9+]$" | head -n 1)
   if [ -n "$TAG" ]; then
     break
